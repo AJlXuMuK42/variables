1bfea6d1cfc697a2cf1c0c9a335547a3a8eb6aed
AJlXuMuK42 <ajlxumuk65@gmail.com>
2024-12-15 21:46:56 +0700

    Add files via upload

---
+++ Pyton_name.py
@@ -0,0 +1,9 @@
+number_of_completed_tasks = 12
+print(number_of_completed_tasks, type(number_of_completed_tasks))
+Number_of_hours_spent = 1.5
+print(Number_of_hours_spent, type(Number_of_hours_spent))
+Course_name = 'Python'
+print(Course_name, type(Course_name))
+Time_per_task = (Number_of_hours_spent / number_of_completed_tasks)
+print(Time_per_task, type(Time_per_task))
+print("Курс:", Course_name,",", "всего задач:", number_of_completed_tasks, ",", "затрачено часов:", Number_of_hours_spent, ",", "среднее время выполения", Time_per_task, "часа")
---
+++ main.py
@@ -0,0 +1,10 @@
+name = 'Ilya'
+print (name, type (name))
+age = 28
+print (age, type (age))
+new_age = age+2
+print (new_age, type (new_age))
+is_student = True
+print (is_student, type (is_student))
+
+
